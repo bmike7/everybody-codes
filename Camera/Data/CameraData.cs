0e107da07fd2fed139ecb701aa99d731f1d2b194
bmike7 <bijlmike3@gmail.com>
2021-02-18 10:46:35 +0100

    CLI: able to read CSV file;

--- Camera/Data/CameraData.cs
+++ Camera/Data/CameraData.cs
@@ -1,13 +1,22 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
 
 namespace Data
 {
     public class CameraRow
     {
+        [Index(0)]
         public string Name { get; set; }
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
+        [Index(1)]
+        public string Latitude { get; set; }
+        [Index(2)]
+        public string Longitude { get; set; }
 
         public void Print()
         {
@@ -19,15 +28,29 @@ namespace Data
     {
         public IEnumerable<CameraRow> GetData()
         {
-            return new List<CameraRow>()
-            {
-                new CameraRow() {Name = "test", Latitude = 4.323, Longitude = 8.44 }
-            };
+            return getData();
         }
 
         public IEnumerable<CameraRow> SearchName(string name)
         {
             throw new System.NotImplementedException();
+        }
+
+        //https://stackoverflow.com/questions/59787783/why-cant-i-convert-from-system-io-streamwriter-to-csvhelper-iserializer
+        private IEnumerable<CameraRow> getData()
+        {
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = true,
+                HeaderValidated = null,
+                MissingFieldFound = null,
+                Delimiter = ";",
+            };
+            using (var reader = new StreamReader("../Data/data/cameras-defb.csv"))
+            using (var csv = new CsvReader(reader, config))
+            {
+                return csv.GetRecords<CameraRow>().ToList();
+            }
         }
     }
 }
