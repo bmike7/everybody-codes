d644ba3314a32267f3fa3b8589fb81f7846218b6
bmike7 <bijlmike3@gmail.com>
2021-02-18 09:26:39 +0100

    START assignment: created dotnet cli application;

---
+++ Camera/CameraCLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CameraCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
